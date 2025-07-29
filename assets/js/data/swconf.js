c6cc4b3081368875b91c7f21857a0803cea187c5
shwetank-dev <shwetank-dev@users.noreply.github.com>
2025-07-29 23:05:29 +0000

    deploy: 891a216cf90a11c87a3e85331fc95bba665a0ef8

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753829651',resources: [
+    cacheName: 'chirpy-1753830329',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
